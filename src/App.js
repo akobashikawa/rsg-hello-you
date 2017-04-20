4cd630b88676e4621764fe1629a972ed6bc2d78b
Rulo Kobashikawa <akobashikawa@gmail.com>
2017-04-20 12:47:59 -0500

    Add base markup

--- src/App.js
+++ src/App.js
@@ -16,10 +16,24 @@ class App extends Component {
         <nav className="navbar navbar-inverse navbar-fixed-top">
           <div className="container">
             <div className="navbar-header">
-              <a className="navbar-brand" href="#">Hello You</a>
+              <a className="navbar-brand" href="./">Hello You</a>
             </div>
           </div>
         </nav>
+
+        <div className="main">
+          <div className="container">
+            <div className="input-group col-xs-4 col-xs-offset-4">
+              <input className="form-control" type="text" placeholder="Your Name"/>
+              <div className="input-group-btn">
+                <button className="btn">Hello</button>
+              </div>
+            </div>
+            <div>
+              <h1>Hello You!</h1>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
