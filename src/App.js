a63a223f99db6048bae1cc28452d5f8aa62c421b
Rulo Kobashikawa <akobashikawa@gmail.com>
2017-04-22 01:58:55 -0500

    Add logo

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ window.jQuery = $;
 require('bootstrap');
 
 import HelloYou from './HelloYou';
+import logo from './logo.svg';
 import './App.css';
 
 const Navbar = (props) => (
   <nav className="navbar navbar-inverse navbar-fixed-top">
     <div className="container">
       <div className="navbar-header">
-        <a className="navbar-brand" href="./">{props.title}</a>
+        <a className="navbar-brand" href="./">
+          <img src={logo} className="App-logo" alt="logo" />
+          {props.title}
+        </a>
       </div>
     </div>
   </nav>
